01ad3c69794901c17aa9eb8d3c2fca6728f7f0f8
asif <md.arahmanasif@gmail.com>
2026-08-03 21:08:57 +0600

    Update forms.py

--- file_shareapp/forms.py
+++ file_shareapp/forms.py
@@ -10,3 +10,6 @@ class AuthenticatedFileForm(forms.ModelForm):
     class Meta:
         model = File
         fields = ('uploaded_file', 'title', 'expiry_date',)
+        widgets = {
+            'expiry_date': forms.DateInput(attrs={'type': 'date'}),
+        }
